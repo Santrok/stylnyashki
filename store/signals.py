33d7a898efefffee414e81d9b388cf78bb27c2ec
Santrok <santr0k@yandex.ru>
2026-04-21 22:49:55 +0300

    добалено удаление файла при удалении записи

--- store/signals.py
+++ store/signals.py
@@ -1,8 +1,10 @@
+import os
+
 from django.contrib.auth import get_user_model
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_delete
 from django.dispatch import receiver
 
-from .models import Profile
+from .models import Profile, Product
 
 User = get_user_model()
 
@@ -11,3 +13,14 @@ User = get_user_model()
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
+
+@receiver(pre_delete, sender=Product)
+def product_image_delete(sender, instance, **kwargs):
+    """
+    Удаление файлов и папок, перед удалением экземпляра объявления.
+    """
+    image_folder = os.path.dirname(instance.image.path) if instance.image else None
+    instance.image.delete(False)
+    if image_folder and os.path.exists(image_folder) and os.path.isdir(image_folder):
+        if not os.listdir(image_folder):
+            os.rmdir(image_folder)
